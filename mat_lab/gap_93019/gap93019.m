1c31db0a276e64c2cce0b749e3c7db9dad214fb0
João Laranjo <joao.laranjo@ua.pt>
2019-11-12 22:08:41 +0000

    MatLab GAP Done

---
+++ mat_lab/gap_93019/gap93019.m
@@ -0,0 +1,31 @@
+%% Seed 93019 All Permutations 
+
+clear;
+clc;
+
+T = readtable("gap_result_93019.txt")
+
+%teste = T.Var1(1)
+n = T.Var3;
+visited = T.Var5;
+t = T.Var7;
+logt = log10(t);
+
+
+figure
+plot(n, t, "-or", "LineWidth", 1)
+title('GenAllPerm 93019')
+xlabel('n')
+ylabel('t(seg)')
+xlim([-1 15])
+ylim([-20 2000])
+
+figure
+plot(n, t, "-or", "LineWidth", 1)
+title('GenAllPerm 93019')
+xlabel('n')
+ylabel('t(seg)')
+set(gca, 'YScale', 'log')
+xlim([-1 15])
+ylim([-20 2000])
+
