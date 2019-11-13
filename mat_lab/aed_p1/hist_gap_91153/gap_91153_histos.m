199204ab29759f9dfb234defffa2d2b7b52b0b97
João Laranjo <joao.laranjo@ua.pt>
2019-11-13 20:13:32 +0000

    Additions Matlab to Relatorio

--- mat_lab/aed_p1/hist_gap_91153/gap_91153_histos.m
+++ mat_lab/aed_p1/hist_gap_91153/gap_91153_histos.m
@@ -20,74 +20,92 @@ T14 = readtable("histo_cost_14.txt");
 n_costs = T1.x0;
 [lin, col] = size(n_costs);
 costs = [1 : 1 : lin];
-plot(costs, n_costs)
+plot_1 = plot(costs, n_costs)
 hold on
 
 n_costs = T2.x0;
 [lin, col] = size(n_costs);
 costs = [1 : 1 : lin];
-plot(costs, n_costs)
+plot_2 = plot(costs, n_costs)
 hold on
 
 n_costs = T3.x0;
 [lin, col] = size(n_costs);
 costs = [1 : 1 : lin];
-plot(costs, n_costs)
+plot_3 = plot(costs, n_costs)
 hold on
 
 n_costs = T4.x0;
 [lin, col] = size(n_costs);
 costs = [1 : 1 : lin];
-plot(costs, n_costs)
+plot_4 = plot(costs, n_costs)
 hold on
 
 n_costs = T5.x0;
 [lin, col] = size(n_costs);
 costs = [1 : 1 : lin];
-plot(costs, n_costs)
+plot_5 = plot(costs, n_costs)
 hold on
 
 n_costs = T6.x0;
 [lin, col] = size(n_costs);
 costs = [1 : 1 : lin];
-plot(costs, n_costs)
+plot_6 = plot(costs, n_costs)
 hold on
 
 n_costs = T7.x0;
 [lin, col] = size(n_costs);
 costs = [1 : 1 : lin];
-plot(costs, n_costs)
+plot_7 = plot(costs, n_costs)
 hold on
 
 n_costs = T8.x0;
 [lin, col] = size(n_costs);
 costs = [1 : 1 : lin];
-plot(costs, n_costs)
+plot_8 = plot(costs, n_costs)
 hold on
 
 n_costs = T9.x0;
 [lin, col] = size(n_costs);
 costs = [1 : 1 : lin];
-plot(costs, n_costs)
+plot_9 = plot(costs, n_costs)
 hold on
 
 n_costs = T10.x0;
 [lin, col] = size(n_costs);
 costs = [1 : 1 : lin];
-plot(costs, n_costs)
+plot_10 = plot(costs, n_costs)
 hold on
 
 n_costs = T11.x0;
 [lin, col] = size(n_costs);
 costs = [1 : 1 : lin];
-plot(costs, n_costs)
+plot_11 = plot(costs, n_costs)
+hold on
+
+n_costs = T12.x0;
+[lin, col] = size(n_costs);
+costs = [1 : 1 : lin];
+plot_12 = plot(costs, n_costs)
+hold on
+
+n_costs = T13.x0;
+[lin, col] = size(n_costs);
+costs = [1 : 1 : lin];
+plot_13 = plot(costs, n_costs)
+hold on
+
+n_costs = T14.x0;
+[lin, col] = size(n_costs);
+costs = [1 : 1 : lin];
+plot_14 = plot(costs, n_costs)
 hold on
 
 title('GAP 91153')
 xlabel('Costs')
-ylabel('Uses (n)')
+ylabel('Uses nOccurrences')
+%set(gca, 'YScale', 'log')
 
-
-
+legend([plot_1, plot_2, plot_3, plot_4, plot_5, plot_6, plot_7, plot_8, plot_9, plot_10, plot_11, plot_12, plot_13, plot_14], "n=1", "n=2", "n=3", "n=4", "n=5", "n=6", "n=7", "n=8", "n=9",  "n=10", "n=11", "n=12",  "n=13", "n=14")
 
 
