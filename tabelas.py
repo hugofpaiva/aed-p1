269ec7c032781b2505a3702db666132e898c32bb
hugofpaiva <hugofpaiva@ua.pt>
2019-11-24 15:16:38 +0000

    Tabela almost done

--- tabelas.py
+++ tabelas.py
@@ -1,21 +1,28 @@
 from prettytable import PrettyTable
+import os
 
-x = PrettyTable()
 
-x.field_names = ["Seed", "n", "n Visited",
-                 "CPU Time", "Minimum Cost", "Maximum Cost"]
 try:
-
-    f = open("dados_tabela/bb_minmax_result_93195.txt", "r")
-    line = f.readline()
-    cnt = 1
-    while line:
-        values = line.strip("\t")
-        x.add_row([values[0], values[1], values[2], values[3], values[4], values[5]])
+    for filename in os.listdir('dados_tabela'):
+        f = open("dados_tabela/"+filename, "r")
         line = f.readline()
-        
+        x = PrettyTable()
+        title_table = filename[0:len(filename)-4]
+        x.title = title_table
+        x.field_names = ["Seed", "n", "n Visited",
+                 "CPU Time", "Minimum Cost", "Maximum Cost"]
+        while line:
+            values = line.strip("\n").split("\t")
+            while("" in values):
+                values.remove("")
+            
+            x.add_row([values[0], values[1], values[2],
+                values[3], values[4], values[5]])
+            line = f.readline()
+        print(x)
+        print("\n")
 
 
 finally:
     f.close()
-    print(x)
+    
